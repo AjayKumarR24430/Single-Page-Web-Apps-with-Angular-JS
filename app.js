a633025d9ad9b6b8a92c31a0531cc71200761b25
Ajay Kumar R <ajaykumarr3024@gmail.com>
2020-08-03 19:13:06 +0530

    Name calculator

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-(function() {
+(function () {
     'use strict';
-
-    angular.module('myFirstApp',[] )
-
-    .controller('MyFirstController', function ($scope){
-        $scope.name="Ajay Kumar";
-        $scope.sayHello= function () {
-            return "Hello Everyone!"
+    
+    angular.module('NameCalculator', [])
+    
+    .controller('NameCalculatorController', function ($scope) {
+      $scope.name = "";
+      $scope.totalValue = 0;
+    
+      $scope.displayNumeric = function () {
+        var totalNameValue = calculateNumericForString($scope.name);
+        $scope.totalValue = totalNameValue;
+      };
+    
+    
+      function calculateNumericForString(string) {
+        var totalStringValue = 0;
+        for (var i = 0; i < string.length; i++) {
+          totalStringValue += string.charCodeAt(i);
         }
+    
+        return totalStringValue;
+      }
+    
     });
-
-})();
+    
+    
+    })();
+    
